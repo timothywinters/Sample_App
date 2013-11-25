1924488b6f1401588def27a1b3ff2b471367555f
Timothy Winters <timothy@timothywinters.com>
2013-11-24 17:05:21 -0800

    Finish sign up

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131113235908) do
+ActiveRecord::Schema.define(version: 20131125004404) do
 
   create_table "users", force: true do |t|
     t.string   "name"
